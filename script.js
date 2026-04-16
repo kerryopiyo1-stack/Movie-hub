ceea271e6598b9ffd1afc1e0789124e0c282ae40
kerry opiyo <kerryopiyo0758@gmail.com>
2026-04-16 18:36:54 +0300

    added

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //API_KEY
-const API_KEY = "8bea28b7fc513253b2e5d30bec9123c9";
+const API_KEY = "";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+//selecting HTML elements DOM selection)
+const  moviesGrid = document.getElementById("movies-container");
+const searchInput = document.getElementById("search-input");
+
+//function for fetching movies
+async function getMovies (url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayMovies(data.results);
+    } catch (error) {
+        console.error("Error fetching movies:", error);
+    }
+}
