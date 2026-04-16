3ab2a55014b55d7b980c55cded20daff2a3257b7
kerry opiyo <kerryopiyo0758@gmail.com>
2026-04-16 22:39:01 +0300

    added

--- script.js
+++ script.js
@@ -32,4 +32,14 @@ const image = poster_path ? IMAGE_BASE_URL + poster_path : 'https://via.placehol
 //creating a moviecard element
 const movieCard = document.createElement("div")
 movieCard.className = "bg-green-800 rounded-lg shadow-lg hover:scale-105 transform transition duration-300";
+movieCard.innerHTML = 
+<div classs= "relative group">
+    <img src="${image}" alt="${title}" class="w-full h-64 object-cover rounded-t-lg">
+    <div class="absolute inset-0 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100 transition duration-300 flex flex-col justify-center items-center text-center p-4 rounded-b-lg">
+        <h2 class="text-lg font-bold mb-2">${title}</h2>
+        <p class="text-sm mb-4">${overview}</p>
+        <span class="bg-red-500 text-white px-2 py-1 rounded">${vote_average}</span>
+    </div>
+
+</div>
 
