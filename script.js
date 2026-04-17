b04432f65abddba624f5207a07a5b09dd95aa1fb
kerry opiyo <kerryopiyo0758@gmail.com>
2026-04-17 10:22:51 +0300

    added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //API_KEY
-const API_KEY = "";
+const API_KEY = "8bea28b7fc513253b2e5d30bec9123c9";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
@@ -22,24 +22,45 @@ async function getMovies (url) {
 function displayMovies(movies) {
     moviesGrid.innerHTML = "";
     movies.forEach(movie => {  
- 
+        const { poster_path, title, vote_average } = movie;
+        
+        //looping through movies 
+        const image = poster_path ? IMAGE_BASE_URL + poster_path : 'https://via.placeholder.com/300x450';
+
+        //creating a moviecard element
+        const movieCard = document.createElement("div");
+        movieCard.className = "bg-green-800 rounded-lg shadow-lg hover:scale-105 transform transition duration-300";
+        movieCard.innerHTML = `
+<div class="relative group">
+    <img src="${image}" alt="${title}" class="w-full h-64 object-cover rounded-t-lg"/>
+    <div class="absolute inset-0 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100 transition duration-300 flex flex-col justify-center items-center text-center p-4 rounded-b-lg">
+       <button class="bg-red-500 px-4 py-2 rounded">View</button>
+    </div>
+    <div class="p-4">
+        <h2 class="text-lg font-bold mb-2">${title}</h2>
+        <p class="text-yellow-400 mb-2">${vote_average}</p>
+    </div>
+</div>
+        `;
+        
+        //Adding cards to the page 
+        moviesGrid.appendChild(movieCard);
     });
 }
+//loading movies  automatically when the page loads
 
-//looping through movies 
-const image = poster_path ? IMAGE_BASE_URL + poster_path : 'https://via.placeholder.com/300x450';
 
-//creating a moviecard element
-const movieCard = document.createElement("div")
-movieCard.className = "bg-green-800 rounded-lg shadow-lg hover:scale-105 transform transition duration-300";
-movieCard.innerHTML = 
-<div classs= "relative group">
-    <img src="${image}" alt="${title}" class="w-full h-64 object-cover rounded-t-lg">
-    <div class="absolute inset-0 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100 transition duration-300 flex flex-col justify-center items-center text-center p-4 rounded-b-lg">
-        <h2 class="text-lg font-bold mb-2">${title}</h2>
-        <p class="text-sm mb-4">${overview}</p>
-        <span class="bg-red-500 text-white px-2 py-1 rounded">${vote_average}</span>
-    </div>
+//
 
-</div>
+
+
+
+
+
+
+    
+
+
+
+
 
